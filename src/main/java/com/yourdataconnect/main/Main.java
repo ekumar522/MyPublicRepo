1a994924f37ee74ce6f68c3f518440b696fe1101
ekumar522 <saiegalapati@gmail.com>
2020-07-29 17:13:57 +0530

    Update Main.java

--- src/main/java/com/yourdataconnect/main/Main.java
+++ src/main/java/com/yourdataconnect/main/Main.java
@@ -1,25 +1,15 @@
 package com.yourdataconnect.main;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.sforce.ws.ConnectionException;
-import com.yourdataconnect.p360.P360;
-import com.yourdataconnect.p360.Ydc;
 
 
 
 public class Main {
-	private final static Logger log = LoggerFactory.getLogger(Main.class);
-	public static void main(String[] args) throws IOException, ConnectionException {
+		public static void main(String[] args) throws IOException, ConnectionException {
 		
 	
-			log.info("+++++++++++++++++++++ Integration Completed +++++++++++++++++++++");
+			System.out.println("+++++++++++++++++++++ Integration Completed +++++++++++++++++++++");
 				
 
 	}	
